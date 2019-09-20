b2393031ee09a3705fd7e088bbb469f98a2c5a91
jambis <imjaamees@gmail.com>
2019-09-20 06:03:40 -0500

    Added some api requests to build a deck of 21 cards and show them

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [newDeck, setNewDeck] = useState(null);
+  useEffect(() => {
+    axios
+      .get("https://deckofcardsapi.com/api/deck/new/shuffle/")
+      .then(res => {
+        setNewDeck(res.data);
+      })
+      .catch(err => console.error(err));
+  }, []);
+
+  const [cardsArr, setCardsArr] = useState([]);
+  const [imagesArr, setImagesArr] = useState([]);
+  useEffect(() => {
+    if (newDeck) {
+      axios
+        .get(
+          `https://deckofcardsapi.com/api/deck/${newDeck.deck_id}/draw/?count=21`
+        )
+        .then(res => {
+          res.data.cards.forEach(element => {
+            setCardsArr(codes => [...codes, element.code]);
+          });
+          res.data.cards.forEach(element => {
+            setImagesArr(images => [...images, element.image]);
+          });
+        })
+        .catch(err => console.error(err));
+    }
+  }, [newDeck]);
+
+  const [shortDeck, setShortDeck] = useState(null);
+  useEffect(() => {
+    if (cardsArr.length === 21) {
+      axios
+        .get(
+          `https://deckofcardsapi.com/api/deck/new/shuffle/?cards=${cardsArr.join(
+            ","
+          )}`
+        )
+        .then(res => setShortDeck(res.data))
+        .catch(err => console.error(err));
+    }
+  }, [cardsArr]);
+
+  if (shortDeck) {
+    axios
+      .get(
+        `https://deckofcardsapi.com/api/deck/${shortDeck.deck_id}/draw/?count=1`
+      )
+      .then(res => {
+        //console.log(res.data);
+        axios
+          .get(
+            `https://deckofcardsapi.com/api/deck/${shortDeck.deck_id}/pile/pile1/add/?cards=${res.data.cards[0].code}`
+          )
+          .then(res => console.log(res.data));
+      })
+      .catch(err => console.error(err));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {imagesArr.length === 21 ? (
+        imagesArr.map(image => <img src={`${image}`} />)
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
